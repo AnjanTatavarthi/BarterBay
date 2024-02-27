d91edacfea60a5ec45160949a220e9b5cd1d875f
AnjanTatavarthi <anjantatavarthi1997@gmail.com>
2024-02-26 19:24:38 -0800

    Created the basic structure for the backend

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+app.get("/status", (request, response) => {
+    const status = {
+        "Status": "Running"
+    };
+    response.send(status);
+});
